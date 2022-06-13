0712139c8ed3218f31ef494ffe00d5ac3ac4e5ca
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-06-13 16:19:20 +0530

    profile picture

--- data.py
+++ data.py
@@ -1,16 +1,17 @@
 from faker import Faker
 import json
-
+import random
 
 faker = Faker()
 
 def generate_profile(seed=None, amount=1):
-    if seed:
-        Faker.seed(seed)
+    seed = seed or random.randint(0, 1000000)
+    Faker.seed(seed)
     ret = []
     for i in range(int(amount)):
         profile = faker.profile()
         profile['phone'] = faker.phone_number()
+        profile['picture'] = "https://derpy-peeps.vercel.app/image?seed=" + str(seed)
         ret.append(profile)
     return ret
 
